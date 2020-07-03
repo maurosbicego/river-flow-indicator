8a0b65a8f940538f9536b014600eab1719251c11
maurosbicego <mauro@sbicego.ch>
2020-07-03 22:34:58 +0200

    Display flow rate

--- script.py
+++ script.py
@@ -9,8 +9,8 @@ threshold = config["threshold"]
 endpoint = "https://api.existenz.ch/apiv1/hydro/latest?locations=" + station + "&parameters=flow&format=table&app=https%3A%2F%2Fgithub.com%2Fmaurosbicego%2Friver-flow-indicator"
 
 flow = r.get(endpoint).json()["payload"][0]["val"]
-showmessage("ok",1)
+showmessage(str(flow),1)
 if flow > threshold:
-    state = 0
+    showmessage("Aare nicht befahrbar",2)
 else:
-    state = 1
+    showmessage("Aare befahrbar",2)
