456ebd1967d8dd7ef72a2b20b5cb4ac5a09a78b4
maurosbicego <mauro@sbicego.ch>
2020-08-07 13:18:30 +0200

    Changed text & added temperature

--- script.py
+++ script.py
@@ -13,13 +13,15 @@ with open("config.json") as file:
     config = json.load(file)
 station = str(config["station_id"])
 threshold = config["threshold"]
-endpoint = "https://api.existenz.ch/apiv1/hydro/latest?locations=" + station + "&parameters=flow&format=table&app=https%3A%2F%2Fgithub.com%2Fmaurosbicego%2Friver-flow-indicator"
+endpoint = "https://api.existenz.ch/apiv1/hydro/latest?locations=" + station + "&parameters=flow,temperature&format=table&app=https%3A%2F%2Fgithub.com%2Fmaurosbicego%2Friver-flow-indicator"
 
-flow = r.get(endpoint).json()["payload"][0]["val"]
+data = r.get(endpoint).json()["payload"]
+flow = data[0]["val"]
+temperature = [1]["val"]
 lcd_init()
-showmessage(str(flow),1)
+showmessage(str(flow) + "m³/s, " + str(temperature) + " °C",1)
 if flow > threshold:
-    showmessage("Aare nicht befahrbar",2)
+    showmessage("Nicht befahrbar",2)
     GPIO.output(red, GPIO.HIGH)
     GPIO.output(green, GPIO.LOW)
 else:
